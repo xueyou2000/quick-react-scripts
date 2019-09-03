413ad6a3e3e252545cd1e1e971a8eb7a082712dd
XueYou <xueyoucd@gmail.com>
2019-09-03 17:15:50 +0800

    build: 迁移测试包到@testing-library/react

--- scripts/test.js
+++ scripts/test.js
@@ -7,17 +7,17 @@ module.exports = async (cmd) => {
     let argv = [];
     const jestConfig = {
         rootDir: PATHS.currentDirectory,
-        setupFilesAfterEnv: [require.resolve("react-testing-library/cleanup-after-each")],
+        setupFilesAfterEnv: [require.resolve("@testing-library/react/cleanup-after-each")],
         preset: "ts-jest",
         testMatch: ["<rootDir>/tests/**/*.(spec|test).ts?(x)"],
         moduleNameMapper: {
             "\\.(css|scss)$": require.resolve("identity-obj-proxy"),
-            "^.+\\.svg$": require.resolve("jest-svg-transformer")
+            "^.+\\.svg$": require.resolve("jest-svg-transformer"),
         },
         transform: {
             "^.+\\.js$": require.resolve("../config/jest/babelTransform"),
-            "^.+\\.(ts|tsx)$": require.resolve("ts-jest")
-        }
+            "^.+\\.(ts|tsx)$": require.resolve("ts-jest"),
+        },
     };
     const overrides = Object.assign({}, jestConfig, require(path.resolve(PATHS.currentDirectory, "package.json")).jest);
 
